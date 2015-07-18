fedd77bff9e05361ab6631bd34a52a11b89f405f
Will Bell <willbell@gmail.com>
2015-07-18 07:25:34 +0100

    prune controller_test

--- test/controllers/days_controller_test.rb
+++ test/controllers/days_controller_test.rb
@@ -6,18 +6,8 @@ class DaysControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
-
   test "should get new" do
     get :new
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit
     assert_response :success
   end
 
