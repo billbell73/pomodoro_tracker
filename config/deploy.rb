d4000ceaa6b7fa0d7f59a0a343a6dea2f3d7421a
Will Bell <willbell@gmail.com>
2015-08-07 18:04:50 +0100

    add restart task to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,6 +41,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # set :keep_releases, 5
 
 namespace :deploy do
+	after 'deploy:publishing', 'deploy:restart'
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
